c8680e41a898b666d37c8e49c280155667bed285
tienhoah <nickhoang11@gmail.com>
2018-08-22 03:11:53 +0000

    add milestones table

---
+++ migrations/20180822025318_create_milestones.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('milestones', function(table) {
+    table.increments();
+    table.string('description');
+    table.date('date_achieved');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('milestones');
+};
